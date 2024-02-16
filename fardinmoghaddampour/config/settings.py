da5e2aa0133662f84b8227e947c2aec0aad34a65
Fardin_Zand <officialfardinzand@gmail.com>
2024-02-16 16:54:23 +0330

    Edit two file ...

--- fardinmoghaddampour/config/settings.py
+++ fardinmoghaddampour/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "config.musicblog.apps.MusicblogConfig"
+    'musicblog'
 ]
 
 MIDDLEWARE = [
